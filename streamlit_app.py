bd7fd98538d5aeff4f8318d3b1472c38bfe5fcf5
waiz python developer <148198099+waizkhalil@users.noreply.github.com>
2026-09-25 21:35:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.markdown(
             font-size: 16px;
             border-radius: 8px;
             border: 1px solid #ccc;
-            background-color: #f5f5f5;
+            background-color: #1B1B40;
             cursor: pointer;
         ">
             ← Back
